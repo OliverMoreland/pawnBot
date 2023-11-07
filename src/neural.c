905c2ab0598be18f195ace472992fee625b610be
3TH3R3AL <etherealfire42@gmail.com>
2023-11-07 10:30:24 -0500

    transfer

--- src/neural.c
+++ src/neural.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <stdio.h>
 
 #define PI 3.141592653589793238462
 
@@ -25,7 +25,9 @@ typedef struct network {
     neuron  hidden_layers[NUM_HIDDEN_LAYERS-1][NUM_HIDDEN_NEURONS];
     neuron  output_layer[NUM_OUTPUTS];
 } network;
-network current_network;
+
+
+
 double r2()
 {
     return (double)rand() / (double)RAND_MAX ;
@@ -37,32 +39,54 @@ double weight_init_value(int index){
         return sqrt(-2*log(r2()))*sin(2*PI*r2());
     }
 }
-int init_network(network network){
+int init_network(network *network){
+    
     double INIT_MULTIPLIER = sqrt(2.0/NUM_INPUTS);
     for (int i = 0; i < NUM_HIDDEN_NEURONS; i++)
     {
-        network.input_layer[i].bias = 0;
+        network->input_layer[i].bias = 0;
         for(int j = 0; j < NUM_INPUTS;j++){
-            network.input_layer[i].weights[j] = weight_init_value(j) * INIT_MULTIPLIER;
+            network->input_layer[i].weights[j] = weight_init_value(j) * INIT_MULTIPLIER;
         }
     }
     for (int i = 0; i < NUM_HIDDEN_LAYERS-1; i++)
     {
         for (int j = 0; j < NUM_HIDDEN_NEURONS; j++)
         {
-            network.hidden_layers[i][j].bias = 0;
+            network->hidden_layers[i][j].bias = 0;
             for (int k = 0; k < NUM_HIDDEN_NEURONS; k++)
             {
-                network.hidden_layers[i][j].weights[k] = weight_init_value(k) * INIT_MULTIPLIER;
+                network->hidden_layers[i][j].weights[k] = weight_init_value(k) * INIT_MULTIPLIER;
             }
             
         }
         
     }
-    
-    
+    for (int i = 0; i < NUM_OUTPUTS; i++)
+    {
+        network->output_layer[i].bias = 0;
+        for (int j = 0; j < NUM_HIDDEN_NEURONS; j++)
+        {
+            network->output_layer[i].weights[j] = weight_init_value(j) * INIT_MULTIPLIER;
+        }
+        
+    }
 
 }
+
+
 int main(){
     srand(time(NULL));  
+
+    network *current_network = malloc(sizeof(network));
+    network *next_network = malloc(sizeof(network));
+
+    FILE *network_file = fopen("../assets/network", "r+");
+    fread(current_network,sizeof(network),1,network_file);
+    long size = ftell(network_file);
+    if (0 == size) {
+        init_network(current_network);
+    }
+    printf("%f",current_network->hidden_layers[0][1].weights[0]);
+    fwrite(current_network,sizeof(network),1,network_file);
 }
